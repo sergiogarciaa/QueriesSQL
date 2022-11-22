60beb1fba4cc585fa0afce11c3183f9d63be4305
Sergio García Barrera <98455070+sergiogarciaa@users.noreply.github.com>
2022-11-22 09:15:53 +0100

    Add files via upload

---
+++ García Barrera-Sergio_03DBQF5date.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE Sheet1(Column_1 VARCHAR(10),Column_2 VARCHAR(10),Column_3 VARCHAR(10));
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('QUESTIONS','RESPONSES: La queries tienen que ser válidas siempre -independientemente del año en el que estés. Para eso partimos siempre de funciones como curdate() y nunca usamos fechas literales, fijas.','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('','Para conectar rápido, http://sql.ldtsynergy.com','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('Si un alquiler vence dentro de 9 días, ¿en qué fecha vence?','SELECT ADDDATE(CURDATE(), INTERVAL 9 DAY)','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('El jueves próximo, ¿en qué cae?','SELECT IF(WEEKDAY(CURDATE()) < 3, DATE_ADD(CURDATE(), INTERVAL 3 - WEEKDAY(CURDATE())DAY), DATE_ADD(CURDATE(), INTERVAL 10 - WEEKDAY(CURDATE()) DAY))','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('¿En qué fecha cae el primer fin de semana del mes que viene? -Que todos los días sean de ese mes','SELECT DATE_ADD(CURDATE(), INTERVAL 5 - WEEKDAY(CURDATE()) DAY)','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('Si hay reunión los terceros lunes de mes, ¿Cuándo es -o ha sido- la de este mes?','SELECT IF(WEEKDAY(CONCAT(  YEAR(CURDATE()), '-', MONTH(CURDATE()), '-', 1)  ) = 0,
+DATE_ADD( CONCAT( YEAR(CURDATE()), '-', MONTH(CURDATE()), '-', 1) , INTERVAL 14 DAY) ,
+DATE_ADD( CONCAT( YEAR(CURDATE()), '-', MONTH(CURDATE()), '-', 1)   , INTERVAL 21 - WEEKDAY(CONCAT(YEAR(CURDATE()), '-', MONTH(CURDATE()), '-', 1) ) DAY))','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('Repite los anteriores poniendo las fechas en formato español con 2-2-4 dígitos: dd-mm-yyyy','SELECT DATE_FORMAT( IF(WEEKDAY(CONCAT(  YEAR(CURDATE()), '-', MONTH(CURDATE()), '-', 1)  ) = 0,
+DATE_ADD( CONCAT( YEAR(CURDATE()), '-', MONTH(CURDATE()), '-', 1) , INTERVAL 14 DAY) ,
+DATE_ADD( CONCAT( YEAR(CURDATE()), '-', MONTH(CURDATE()), '-', 1)   , INTERVAL 21 - WEEKDAY(CONCAT(YEAR(CURDATE()), '-', MONTH(CURDATE()), '-', 1) ) DAY)),
+
+ '%d %m %y')','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('¿Cuánto tiempo queda hasta que acaben hoy las clases?','SELECT TIMEDIFF(CURRENT_TIME(), '12:45:00')','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('AYUDA! Si quieres, mira cómo se usan las funciones en este ejercicio:','https://share.voomly.com/v/ClP2tIE9W','');
+INSERT INTO Sheet1 (Column_1,Column_2,Column_3) VALUES ('Si un alquiler vence dentro de 9 días, ¿en qué fecha vence?','SELECT ADDDATE(CURDATE(), INTERVAL 9 DAY)','');
---
+++ García Barrera-Sergio_03DBQFGroupBy3.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE Sheet1(QUESTIONS VARCHAR(10),RESPONSES VARCHAR(10));
+INSERT INTO Sheet1 (QUESTIONS,RESPONSES) VALUES ('BASE DE DATOS   estudio','Para conectar rápido, http://sql.ldtsynergy.com');
+INSERT INTO Sheet1 (QUESTIONS,RESPONSES) VALUES ('Número de horas de estudio acumuladas en cada perfil (que aparezcan todos) durante Octubre.','SELECT cad_mes, ush_horas, per_descripcion
+FROM calendario_dias RIGHT JOIN usuarios_horas ON (cad_id=ush_dia) RIGHT JOIN usuarios ON (ush_usuario=usu_id)  RIGHT JOIN perfiles ON (usu_perfil = per_id)
+WHERE cad_mes = 10
+GROUP BY per_id
+');
+INSERT INTO Sheet1 (QUESTIONS,RESPONSES) VALUES ('Listado de todas las salas con los perfiles indicando cuántos estudiantes hay de ese perfil y en esa sala. Si no hay ninguno, que aparezca "NINGUNO"','SELECT sal_descripcion, per_descripcion,
+IF(COUNT(usu_id) = 0,'NINGUNO', COUNT(usu_id))
+FROM salas LEFT JOIN usuarios ON (sal_id = usu_sala) LEFT JOIN perfiles ON (usu_perfil = per_id)
+GROUP  BY sal_id, per_id');
+INSERT INTO Sheet1 (QUESTIONS,RESPONSES) VALUES ('Listado de todos los alumnos y el número total de veces que ha ido a estudiar.','SELECT concat(usu_nombre, " ", usu_apellidos), COUNT(usu_id)
+FROM usuarios RIGHT JOIN usuarios_horas ON (usu_id=ush_usuario)
+GROUP BY usu_id');
+INSERT INTO Sheet1 (QUESTIONS,RESPONSES) VALUES ('Cuando un alumno va a estudiar un día, ¿cuánto tiempo estudia en promedio? Haz el informe para cada perfil','SELECT AVG(ush_horas), per_descripcion
+FROM perfiles RIGHT JOIN usuarios ON (per_id = usu_perfil )RIGHT JOIN usuarios_horas ON (usu_id=ush_usuario)
+GROUP BY per_id');
+INSERT INTO Sheet1 (QUESTIONS,RESPONSES) VALUES ('Listado de todas las salas y el número de perfiles diferentes que hay en cada una','SELECT sal_descripcion, COUNT(per_id)
+FROM salas LEFT JOIN usuarios ON (sal_id=usu_sala) LEFT JOIN perfiles ON (usu_perfil = per_id)
+GROUP BY sal_id');
